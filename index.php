7d19bd303fa0c24a6d04aede38d354f917b01e15
source_server <source_server@example.com>
2016-06-26 14:54:49 +0000

    modify workspace

--- index.php
+++ index.php
@@ -84,7 +84,7 @@
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <div class="post-preview">
                         <h2 class="post-title">
-                            Edit this for title
+                            Edit this for Header
                         </h2>
                         <h3 class="post-subtitle">
                             Problems look mighty small from 150 miles up
