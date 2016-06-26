91f31311e98863522b4fa3505b559aa239082a73
source_server <source_server@example.com>
2016-06-26 14:48:13 +0000

    modify workspace

--- index.php
+++ index.php
@@ -93,15 +93,7 @@
                     </a>
                     <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 24, 2014</p>
                 </div>
-                <hr>
-                <div class="post-preview">
-                    <a href="post.html">
-                        <h2 class="post-title">
-                            I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
-                        </h2>
-                    </a>
-                    <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 18, 2014</p>
-                </div>
+
             </div>
         </div>
     </div>
