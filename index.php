ff6396d4e25edd0d48f072b6e0a355942cb06bd4
Koung Jian Yu <koung.jian.yu@gmail.com>
2016-06-26 14:55:46 +0000

    =

--- index.php
+++ index.php
@@ -69,9 +69,9 @@
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                     <div class="site-heading">
-                        <h1>Clean Blog</h1>
+                        <h1>Recycling</h1>
                         <hr class="small">
-                        <span class="subheading">Recycling</span>
+                        <span class="subheading">Some Slogan</span>
                     </div>
                 </div>
             </div>
@@ -84,10 +84,10 @@
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <div class="post-preview">
                         <h2 class="post-title">
-                            Edit this for title
+                            Edit this for Header
                         </h2>
                         <h3 class="post-subtitle">
-                            Problems look mighty small from 150 miles up
+                            Edit this for Article Title
                         </h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis interdum odio. Vivamus ac placerat nibh. Sed eu ante id risus fermentum sagittis scelerisque in nibh. Nullam fringilla gravida molestie. Morbi ac nisl ut ex tempor fringilla iaculis sit amet diam. Donec porttitor sodales eros ullamcorper aliquam. Vestibulum commodo interdum turpis, quis volutpat turpis tempor sed. Proin vitae congue velit, mollis mollis diam. Morbi mollis finibus justo, sed pellentesque mauris lacinia sit amet. Nullam et sapien mi. Proin malesuada ut urna vel dictum.
 
 Nulla urna felis, mattis vel odio eu, aliquam pharetra nisl. Vivamus eget urna ligula. Morbi rutrum metus id erat sodales volutpat. Nulla facilisi. Duis quis scelerisque ex. Fusce et lacus id nibh molestie eleifend id in turpis. Sed sed velit a tortor faucibus mollis. Duis facilisis tellus ligula, sit amet semper tortor euismod ac. Vivamus vestibulum orci sed lectus rutrum, non congue metus varius. Nunc bibendum iaculis ligula, nec mattis mi. Integer et tempus est. Curabitur sagittis lorem ac fringilla rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. In dictum sem justo. Etiam ut imperdiet mauris, quis vestibulum diam. Nullam facilisis venenatis purus facilisis rhoncus.
@@ -100,7 +100,7 @@ Phasellus lacinia leo ac dui sagittis, vel congue purus molestie. Nulla semper d
 
     <hr>
 
-    <!-- Footer -->
+<!--  
     <footer>
         <div class="container">
             <div class="row">
@@ -135,7 +135,7 @@ Phasellus lacinia leo ac dui sagittis, vel congue purus molestie. Nulla semper d
                 </div>
             </div>
         </div>
-    </footer>
+    </footer> -->
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
